6e2e2a046bc18ab5bdd0eb7d83553710d05a91ff
Student <example@example.com>
2020-01-17 14:56:55 -0800

    add scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |index|
-  new_product = Product.create!(name: Faker::Space.star, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Name.first_name)
+  new_product = Product.create!(name: Faker::Space.star, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Space.planet)
   10.times do |index|
-    new_review = new_product.reviews.new(author: Faker::Name.name, rating: Faker::Number.number(digits: 1), content_body: Faker::Lorem.sentence(word_count: 13))
+    new_review = new_product.reviews.new(author: Faker::Name.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.sentence(word_count: 13))
     new_review.save
   end
 end
