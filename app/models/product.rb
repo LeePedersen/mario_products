d377e8bd8e399fff7b3b0e1d76f35a95aa89fe37
Student <example@example.com>
2020-01-17 16:33:47 -0800

    more styling, changed venus scope to usa

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   validates :cost, presence: true
   validates :country_of_origin, presence: true
 
-  scope :venus, -> { where(country_of_origin: "Venus") }
+  scope :usa, -> { where(country_of_origin: "United States") }
 
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Review.destroy_all
 
 50.times do |index|
   new_product = Product.create!(name: Faker::Space.star, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Space.planet)
-  10.times do |index|
+  5.times do |index|
     new_review = new_product.reviews.new(author: Faker::Name.name, rating: Faker::Number.between(from: 1, to: 5), content_body: Faker::Lorem.sentence(word_count: 13))
     new_review.save
   end
